fd259a49920034fd2b6581b62d21b93e3f3b4c87
lethanfadlil <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2019-09-12 11:19:54 +0700

    first commit

---
+++ src/Praktikum/Praktek_Single_Dimension_3.java
@@ -0,0 +1,10 @@
+package Praktikum;
+
+public class Praktek_Single_Dimension_3 {
+    public static void main(String[] args) {
+        
+        int bilangan[] = {1,2,3,4,5,6,7,8};
+        
+        System.out.println(bilangan[7]);
+    }
+}
